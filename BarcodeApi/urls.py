8e9dc6f0559a7f3ffe3ea2c0513bdf8d26aca916
Vishav Lakhtia <vishav676@gmail.com>
2021-10-14 23:53:00 +0200

    changes in the url pattern

--- BarcodeApi/urls.py
+++ BarcodeApi/urls.py
@@ -16,9 +16,24 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Erasmus Barcode Application API",
+      default_version='v1',
+      description="API endpoints to modify the database.",
+      contact=openapi.Contact(email="vishav676@gmail.com"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('accounts/login/', admin.site.urls),
     path('api/', include('backend.urls')),
     path('api-token-auth/', views.obtain_auth_token),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
--- backend/urls.py
+++ backend/urls.py
@@ -1,21 +1,5 @@
 from django.urls import path
 from backend import views
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
 
 urlpatterns = [
 
@@ -33,7 +17,5 @@ urlpatterns = [
 
     path('checkingTicket/', views.CheckingTicketRelation.as_view()),
     path('checkingTicket/<str:pk>', views.CheckingTableRelationDetail.as_view()),
-
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
 ]
